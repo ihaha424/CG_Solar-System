1cd5bd9993d8dea293dd000e370e511e6695fbfc
JuHyeongKim99 <90828283+JuHyeongKim99@users.noreply.github.com>
2022-11-01 17:33:09 +0900

    push

--- ThreeJS-Code/main/sketch.js
+++ ThreeJS-Code/main/sketch.js
@@ -1,6 +1,6 @@
 import * as Planet from './src/constants/planets.js';
 import { AU, SIDERAL_DAY, NM_TO_KM, DAY, HOUR,KM } from './src/constants/index.js';
-//import {THREE} from './node_modules/three';
+// import {THREE} from './node_modules/three';
 
 const settings = {
   // Make the loop animated
@@ -9,9 +9,24 @@ const settings = {
   context: "webgl",
   scaleToView: true
 };
+
+
+//camera option(global object)
+var camera
+var window
+var projector, mouse = { x: 0, y: 0 };
+var renderer
+var scene
+var controls
+//value is object distance z(정면에서 보기위해서)
+var value_z = 0
+//move flag(버튼을 연속으로 못누르게 lock)
+var flag = 0
+//rendering ID
+var moveID;
+
 const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
-
 
 window.onload = function init() 
 { 
@@ -63,11 +78,17 @@ window.onload = function init()
   const neptuneMaterial = new THREE.MeshStandardMaterial({ map: neptuneTexture });
   const plutoMaterial = new THREE.MeshStandardMaterial({ map: plutoTexture });
 
+  const labelContainerElem = document.querySelector('#labels');//-DongMin
+
   /*
    * MESH
    */
   const scene = new THREE.Scene();
   const geometry = new THREE.SphereGeometry(1, 32, 16);
+
+  //plants number
+  plants_number = 10;//-DongMin
+  plants_Mesh = [];//-DongMin
 
   const sunMesh = new THREE.Mesh(geometry, sunMaterial);
   sunMesh.position.set(0, 0, 0);
@@ -76,39 +97,40 @@ window.onload = function init()
 
   const mercuryGroup = new THREE.Group();
   const mercuryMesh = new THREE.Mesh(geometry, mercuryMaterial);
-  createPlanet(scene, mercuryMesh, mercuryGroup, 25, 0.8);
+  createPlanet(scene, mercuryMesh, mercuryGroup, 25, 0.8,"MERCURY");//-DongMin
+  plants_Mesh = plants_Mesh.concat(mercuryMesh);//-DongMin
 
   const venusGroup = new THREE.Group();
-  const venusMesh = new THREE.Mesh(geometry, venusMaterial);
-  createPlanet(scene, venusMesh, venusGroup, 28, 0.9);
+  createPlanet(scene, venusMesh, venusGroup, 28, 0.9,"VENUS");//-DongMin
+  plants_Mesh = plants_Mesh.concat(venusMesh);//-DongMin
 
   const earthGroup = new THREE.Group();
-  const earthMesh = new THREE.Mesh(geometry, earthMaterial);
-  createPlanet(scene, earthMesh, earthGroup, 31, 1);
+  createPlanet(scene, earthMesh, earthGroup, 31, 1,"EARTH");//-DongMin
+  plants_Mesh = plants_Mesh.concat(earthMesh);//-DongMin
 
   const marsGroup = new THREE.Group();
-  const marsMesh = new THREE.Mesh(geometry, marsMaterial);
-  createPlanet(scene, marsMesh, marsGroup, 34, 0.8);
+  createPlanet(scene, marsMesh, marsGroup, 34, 0.8,"MARS");//-DongMin
+  plants_Mesh = plants_Mesh.concat(marsMesh);//-DongMin
 
   const jupiterGroup = new THREE.Group();
-  const jupiterMesh = new THREE.Mesh(geometry, jupiterMaterial);
-  createPlanet(scene, jupiterMesh, jupiterGroup, 42, 3.5);
+  createPlanet(scene, jupiterMesh, jupiterGroup, 42, 3.5,"JUPITER");//-DongMin
+  plants_Mesh = plants_Mesh.concat(jupiterMesh);//-DongMin
 
   const saturnGroup = new THREE.Group();
-  const saturnMesh = new THREE.Mesh(geometry, saturnMaterial);
-  createPlanet(scene, saturnMesh, saturnGroup, 50, 2.9);
+  createPlanet(scene, saturnMesh, saturnGroup, 50, 2.9,"SATURN");//-DongMin
+  plants_Mesh = plants_Mesh.concat(saturnMesh);//-DongMin
 
   const uranusGroup = new THREE.Group();
-  const uranusMesh = new THREE.Mesh(geometry, uranusMaterial);
-  createPlanet(scene, uranusMesh, uranusGroup, 56, 1.7);
+  createPlanet(scene, uranusMesh, uranusGroup, 56, 1.7, "URANUS");//-DongMin
+  plants_Mesh = plants_Mesh.concat(uranusMesh);//-DongMin
 
   const neptuneGroup = new THREE.Group();
-  const neptuneMesh = new THREE.Mesh(geometry, neptuneMaterial);
-  createPlanet(scene, neptuneMesh, neptuneGroup, 60, 1.65);
+  createPlanet(scene, neptuneMesh, neptuneGroup, 60, 1.65, "NEPTUNE");//-DongMin
+  plants_Mesh = plants_Mesh.concat(neptuneMesh);//-DongMin
 
   const plutoGroup = new THREE.Group();
-  const plutoMesh = new THREE.Mesh(geometry, plutoMaterial);
-  createPlanet(scene, plutoMesh, plutoGroup, 64, 0.5);
+  createPlanet(scene, plutoMesh, plutoGroup, 64, 0.5,"PLUTO");//-DongMin
+  plants_Mesh = plants_Mesh.concat(plutoMesh);//-DongMin
 
   /*
    * LIGHTING
@@ -177,7 +199,10 @@ window.onload = function init()
 
       //controls.dispose();
       //renderer.dispose();
-  function createPlanet(scene, mesh, group, x, scale) {
+  function createPlanet(scene, mesh, group, x, scale, name) {//-DongMin
+    const elem = document.createElement('div');//-DongMin
+    elem.textContent = name;//-DongMin
+    labelContainerElem.appendChild(elem);//-DongMin
     mesh.position.set(x, 0, 0);
     mesh.scale.setScalar(scale);
     group.add(mesh);
@@ -201,5 +226,114 @@ window.onload = function init()
     scene.add( spotlight );
   });
 }
+
+
+//camera button
+var button_list = [];
+var object_num = [];
+for(i=0; i < plants_number; i++){
+button_list = button_list.concat("Button"+i);
+}
+
+//sun
+document.getElementById(button_list[0]).onclick = function(event){
+value_z = 15;//value_Z는 정면에서 보기 위한 z축의 값
+object_num = 0;
+sunMesh.updateWorldMatrix(true,false);
+var tempV = new THREE.Vector3();
+plants_Mesh[0].getWorldPosition(tempV);
+
+moveCam(tempV.x,tempV.y,tempV.z,tempV.x,tempV.y,tempV.z,plants_Mesh[0]);
+//moveCam(scene.children[object_num].position.x,scene.children[object_num].position.y,scene.children[object_num].position.z,scene.children[object_num].position.x,scene.children[object_num].position.y,scene.children[object_num].position.z);
 };
+
+//plant
+for(i=1;i<plants_number;i++){  
+temp_button = document.getElementById(button_list[i])
+temp_button.button = i;
+temp_button.onclick = function(event){
+//scene.children[5].children[0].position.x
+value_z = 5;//value_Z는 정면에서 보기 위한 z축의 값
+object_num = event.path[0].button;//plant order
+// moveCam(scene.children[object_num].children[0].position.x,scene.children[object_num].children[0].position.y,scene.children[object_num].children[0].position.z,scene.children[object_num].children[0].position.x,scene.children[object_num].children[0].position.y,scene.children[object_num].children[0].position.z);
+
+var tempV = new THREE.Vector3();
+plants_Mesh[object_num].getWorldPosition(tempV);
+
+moveCam(tempV.x,tempV.y,tempV.z,tempV.x,tempV.y,tempV.z,plants_Mesh[object_num]);
+
+};
+}
+document.getElementById("Button_Init").onclick = function(){
+value_z = 0;
+moveCam(0, 30, 0,0,0,0,0);
+};
+
+//////////////////
+
+};
+
+
+//camera moving -DongMin
+function moveCam(eye_x, eye_y, eye_z, target_x, target_y, target_z, Mesh)
+{   
+window.cancelAnimationFrame(moveID);
+if(flag == 1)
+  return;
+//button lock
+flag = 1;
+
+loading_num = 20;
+
+//move eye changeed value(변화량)
+m_e_x = (eye_x - camera.position.x)/loading_num;
+m_e_y = (eye_y - camera.position.y)/loading_num;
+m_e_z = (eye_z - camera.position.z + value_z)/loading_num;
+//move target changeed value(변화량)
+m_t_x = (target_x - controls.target.x)/loading_num;
+m_t_y = (target_y - controls.target.y)/loading_num;
+m_t_z = (target_z - controls.target.z)/loading_num;
+
+// console.log(m_e_x,m_e_y,m_e_z);
+// console.log(m_t_x,m_t_y,m_t_z);
+
+//camera position
+c_x = camera.position.x;
+c_y = camera.position.y;
+c_z = camera.position.z;
+
+//target position
+t_x = controls.target.x;
+t_y = controls.target.y;
+t_z = controls.target.z;
+i = 0;
+
+function move_view(){
+  i++;
+  camera.position.set ( c_x + i*m_e_x, c_y + i*m_e_y, c_z + i*m_e_z  );
+  controls.target.set( t_x + i*m_t_x, t_y + i*m_t_y, t_z + i*m_t_z);
+  renderer.render(scene,camera);
+  controls.update();
+  if(i != loading_num)
+     moveID=window.requestAnimationFrame(move_view);
+  else{
+     window.cancelAnimationFrame(moveID);
+  function move_object(){
+    var tempV = new THREE.Vector3();
+    Mesh.getWorldPosition(tempV);
+    camera.position.set(tempV.x,tempV.y,tempV.z + value_z);
+    controls.target.set(tempV.x,tempV.y,tempV.z);
+    renderer.render(scene,camera);
+    controls.update();
+    moveID=window.requestAnimationFrame(move_object);
+  }
+  if(Mesh != 0){
+    move_object();
+  }
+     //button unlock
+     flag = 0;
+  }
+}
+move_view();
+}
 
